774f2bbf3ed1b8eee01c360ada97d9705ee932bc
Mr.yu <937229974@qq.com>
2019-11-26 16:57:05 +0800

    取消拦截去，使用shiro 权限控制

---
+++ src/main/java/com/st/sys/shiro/dao/User.java
@@ -0,0 +1,41 @@
+package com.st.sys.shiro.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+public class User {
+    private String username;
+    private String password;
+    private Boolean available;
+
+    public User(String username, String password, Boolean available) {
+        this.username = username;
+        this.password = password;
+        this.available = available;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
